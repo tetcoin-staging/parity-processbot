2f0c144ec8ae694ba7b4c8e13371c35e8cdc5b3b
joao-paulo-parity <joao-paulo@parity.io>
2021-04-30 11:42:43 -0300

    wip

--- src/companion.rs
+++ src/companion.rs
@@ -19,6 +19,7 @@ async fn update_companion_repository(
 	contributor: &str,
 	contributor_repo: &str,
 	contributor_branch: &str,
+	qualified_ref: String,
 ) -> Result<String> {
 	let token = github_bot.client.auth_key().await?;
 	let secrets_to_hide = [token.as_str()];
@@ -295,11 +296,11 @@ async fn update_companion_repository(
 			.client
 			.patch(
 				&format!(
-					"{}/repos/{}/{}/git/refs/heads/{}",
+					"{}/repos/{}/{}/git/refs/{}",
 					crate::github_bot::GithubBot::BASE_URL,
-					contributor,
-					contributor_repo,
-					contributor_branch
+					owner,
+					owner_repo,
+					qualified_ref
 				),
 				&serde_json::json!(UpdateRefPayload {
 					sha: (&created_commit.sha).to_owned()
@@ -369,6 +370,7 @@ async fn perform_companion_update(
 	let comp_pr = github_bot.pull_request(&owner, &repo, number).await?;
 
 	if let PullRequest {
+		number,
 		head:
 			Some(Head {
 				ref_field: Some(contributor_branch),
@@ -394,6 +396,7 @@ async fn perform_companion_update(
 			&contributor,
 			&contributor_repo,
 			&contributor_branch,
+			format!("pull/{}/head", number),
 		)
 		.await?;
 
